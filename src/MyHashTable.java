86a61661e92ec5e974f909f0340eb99dd7a960a2
nurbibi10 <154303818+nurbibi10@users.noreply.github.com>
2024-05-14 22:52:52 +0500

    basic

---
+++ src/MyHashTable.java
@@ -0,0 +1,20 @@
+public class MyHashTable<K, V>{
+    private class HashNode<K, V>{
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode<K, V> getNext() {
+            return next;
+        }
+        public HashNode(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{"+ key + " " + value + "}";
+        }
+    }
+}
