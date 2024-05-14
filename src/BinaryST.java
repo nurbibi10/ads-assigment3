4e6e508d187c08ac01af423ca754423a18afa2e8
nurbibi10 <154303818+nurbibi10@users.noreply.github.com>
2024-05-14 23:02:46 +0500

    first commit

---
+++ src/BinaryST.java
@@ -0,0 +1,29 @@
+import org.w3c.dom.Node;
+
+public class BinaryST<K extends Comparable<K>, V> {
+    private Node root;
+    private class Node{
+        private K key;
+        private V val;
+        private Node left, right;
+        public Node(K key, V val){
+            this.key = key;
+            this.val = val;
+        }
+    }
+    public void put(K key, V val){
+
+    }
+
+    public V get(K key){
+
+    }
+
+    public void delete(K key){
+
+    }
+
+    public Iterable<K> iterator(){
+
+    }
+}
