38ed8f067f1bd8f51d55e7f479761075a021f86a
nurbibi10 <154303818+nurbibi10@users.noreply.github.com>
2024-05-14 23:38:06 +0500

    needing methods

--- src/BinaryST.java
+++ src/BinaryST.java
@@ -12,15 +12,73 @@ public class BinaryST<K extends Comparable<K>, V> {
         }
     }
     public void put(K key, V val){
-
+        root = put(root, key, val);
     }
 
-    public V get(K key){
-
+    private Node put(Node current, K key, V val) {
+        if (current == null)
+            return new Node(key, val);
+        int cm = key.compareTo(current.key);
+        if (cm < 0)
+            current.left = put(current.left, key, val);
+        else if (cm > 0)
+            current.right = put(current.right, key, val);
+        else
+            current.val = val;
+        return current;
     }
 
-    public void delete(K key){
+    public V get(K key) {
+        Node current = root;
+        while (current != null) {
+            int cmp = key.compareTo(current.key);
+            if (cmp < 0)
+                current = current.left;
+            else if (cmp > 0)
+                current = current.right;
+            else
+                return current.val;
+        }
+        return null;
+    }
 
+    public void delete(K key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node current, K key) {
+        if (current == null)
+            return null;
+        int cmp = key.compareTo(current.key);
+        if (cmp < 0)
+            current.left  = delete(current.left,  key);
+        else if (cmp > 0)
+            current.right = delete(current.right, key);
+        else {
+            if (current.right == null)
+                return current.left;
+            if (current.left  == null)
+                return current.right;
+            Node t = current;
+            current = Min(t.right);
+            current.right = deletemin(t.right);
+            current.left = t.left;
+        }
+        return current;
+    }
+
+    private Node deletemin(Node current) {
+        if (current.left == null)
+            return current.right;
+        current.left = deletemin(current.left);
+        return current;
+    }
+
+    private Node Min(Node current) {
+        if (current.left == null)
+            return current;
+        else
+            return Min(current.left);
     }
 
     public Iterable<K> iterator(){
