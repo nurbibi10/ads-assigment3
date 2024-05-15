1bd630f8896d7b4890d939a5023cdd853c884026
nurbibi10 <154303818+nurbibi10@users.noreply.github.com>
2024-05-15 21:44:51 +0500

    main

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -1,6 +1,8 @@
 import java.util.Random;
 
 public class MyTestingClass {
+
+
     public static void main(String[] args) {
         MyHashTable<MyTestingClass, String> nabi = new MyHashTable<>();
 
